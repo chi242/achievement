d000f36dbc09700cf4f6819a56fb0204f9caeefd
chi242 <chisatodesuu@gmail.com>
2021-03-30 14:53:39 +0900

    commit

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,7 @@ Route::post('/goals', 'App\Http\Controllers\GoalController@store');
 
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
