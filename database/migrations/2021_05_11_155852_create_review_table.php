a90080532aa506880ff2016c0a565cf93d56a999
chi242 <chisatodesuu@gmail.com>
2021-05-11 16:19:51 +0900

    create migration'review'

--- database/migrations/2021_05_11_155852_create_review_table.php
+++ database/migrations/2021_05_11_155852_create_review_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDiariesTable extends Migration
+class CreateReviewTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class CreateDiariesTable extends Migration
      */
     public function up()
     {
-        Schema::create('diaries', function (Blueprint $table) {
+        Schema::create('review', function (Blueprint $table) {
             $table->id();
-            $table->text('todo');
+            $table->text('review');
+            $table->integer('evaluation');
             $table->timestamps();
         });
     }
@@ -27,6 +28,6 @@ class CreateDiariesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('diaries');
+        Schema::dropIfExists('review');
     }
 }
