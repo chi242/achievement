64534acf3cee988623695018aa1ba423f12d0720
chi242 <chisatodesuu@gmail.com>
2021-08-09 16:26:20 +0900

    improvement Blade

---
+++ resources/views/planlist/show.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Planリスト編集</title>
+        <!-- Fonts -->
+        <link href="secure_asset{{'../../../assets/css/review/create.style.css'}}" rel="stylesheet">
+    </head>
+    <body>
+        <p>Planリスト編集</p>
+    </body>
+</html>
