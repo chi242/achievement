09072beb93cc228b79ecedc720a3e10e6785b425
chi242 <chisatodesuu@gmail.com>
2021-09-29 18:52:19 +0900

    所有権確認

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,8 +32,7 @@
 	        border-style: solid;	
         }
       </style> 
-<meta name="google-site-verification" content="IJ2nGXbCbPEZb5hdzpcyqEmljZv1L3htW6sjZ3Vic78" />
-</head>
+    </head>
     <body>
       <div class="nav justify-content-end m-2">
         <!--ユーザーボタン（ログアウト・ホームボタン）-->
